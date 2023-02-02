282b37b4df734723df20dae4215b545cdd9c0d86
AvarageCodeEnjoyer <111468739+AvarageCodeEnjoyer@users.noreply.github.com>
2023-02-01 18:19:55 -0700

    update fetch and outputs

--- app.js
+++ app.js
@@ -1,22 +1,32 @@
 const submit = document.querySelector('submit')
 const form = document.querySelector('form')
 var result = document.getElementById('result')
+var weatherLocation = document.getElementById('location')
+
 
 
 form.addEventListener('submit', e =>{
   e.preventDefault()
   let country = document.getElementById('country').value
   let city = document.getElementById('city').value  
-  fetch (`https://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=8e39e4bf370892ecca5bd840dcb309c8`)
+  let API_KEY = "8e39e4bf370892ecca5bd840dcb309c8"
+  fetch (`https://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${API_KEY}`)
   .then(response => response.json())
   .then(data => {
-    result.innerHTML = printRes(data)
+    let {outputTemp, outputWeather} = printRes(data)
+    weatherLocation.innerHTML = "hello"
+    result.innerHTML = `${outputTemp} <br> ${outputWeather}`
+    console.log(outputTemp, outputWeather)
   })
-
-  console.log(city)
-  console.log(country)
+  // result.innerHTML = "Don't waste API Calls"
 })
 
 function printRes(data){
-  return data.main.temp
+  let temp = (data.main.temp - 273.15).toFixed(1)
+  let celsius = temp + "°C"
+  let fahrenheit = temp * 9 / 5 + 32 + "℉"
+  let sky = data.weather[0].main
+  let outputTemp = `The Temperature is ${fahrenheit} or ${celsius},` 
+  let outputWeather = `The Sky has ${sky}`
+  return {outputTemp, outputWeather}
 }
